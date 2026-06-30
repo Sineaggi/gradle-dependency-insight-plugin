54c6e9a1a81c811b4d0d7208f8fcec104bb7735a
Clayton Walker <clayton.m.walker@gmail.com>
2026-06-30 07:54:41 -0800

    Simplify plugin alignment

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    id("com.google.protobuf") version "0.10.0" apply false
+    id("com.gradle.plugin-publish") version "2.1.1" apply false
+    id("org.gradle.plugin-compatibility") version "1.0.0" apply false
+}
